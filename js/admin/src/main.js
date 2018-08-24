715501942c9dfaf108232c189ac888a7b429856f
Arda Çebi <ardacebi1@gmail.com>
2018-08-24 20:51:41 +0300

    Add ripple support 248273984720134

--- js/admin/src/main.js
+++ js/admin/src/main.js
@@ -1,3 +1,7 @@
 import app from 'flarum/app';
 
-app.initializers.add('extum/flarum-ext-material', () => {});
+app.initializers.add('extum/flarum-ext-material', () => {
+
+  document.getElementById("Button").className = "Button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
+  
+});
